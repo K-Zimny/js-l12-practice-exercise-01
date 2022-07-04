bfc0e004ef5360d3d25abeb03b80376fe146a342
kzim <k.zimny1994@gmail.com>
2022-07-04 18:41:33 -0500

    Created user login api script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function () {
+  const usersRequest = await fetch("https://randomuser.me/api?results=5");
+  data = await usersRequest.json();
+  console.log(data.results);
+  displayUsers(data.results);
+};
+
+const displayUsers = function (userResults) {
+  randomFolks.innerHTML = "";
+  for (const users of userResults) {
+    const country = users.location.country;
+    const name = users.name.last;
+    const imageUrl = users.picture.medium;
+    console.log(name);
+    const userDiv = document.createElement("div");
+    userDiv.innerHTML = `
+            <h3>${name}</h3>
+            <p>${country}</p>
+            <img src=${imageUrl} alt="User avatar" />
+        `;
+    randomFolks.append(userDiv);
+  }
+};
+
+getData();
